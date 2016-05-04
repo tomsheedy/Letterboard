ad0b710a8a1e32a4d909a8e666ee04263b4c13cc
tomsheedy <thomas2.sheedy@live.uwe.ac.uk>
2016-05-04 18:47:29 +0100

    v1.1
    Introduced shadows and variety of tilt.

--- Letterboard/letterboard.js
+++ Letterboard/letterboard.js
@@ -25,13 +25,33 @@ function wobbleify(str, ishead) {
   }
   arr.forEach(
     function spanify(value) {
-      var rnd = Math.floor(Math.random() * 15);
-      if (rnd === 0 || rnd === 1) {
-        spans = spans + "<span class='" + cls + " rotleft'>" + value + "</span>";
-      } else if (rnd === 2 || rnd === 3) {
-        spans = spans + "<span class='" + cls + " rotright'>" + value + "</span>";
-      } else {
+      var rnd = Math.floor(Math.random() * 25);
+      if (value === " ") {
         spans = spans + "<span class='" + cls + "'>" + value + "</span>";
+      } else {
+        if (rnd === 0) {
+          spans = spans + "<span class='" + cls + " rot r5'>" + value + "</span>";
+        } else if (rnd === 1) {
+          spans = spans + "<span class='" + cls + " rot r4'>" + value + "</span>";
+        } else if (rnd === 2) {
+          spans = spans + "<span class='" + cls + " rot r3'>" + value + "</span>";
+        } else if (rnd === 3) {
+          spans = spans + "<span class='" + cls + " rot r2'>" + value + "</span>";
+        } else if (rnd === 4) {
+          spans = spans + "<span class='" + cls + " rot r1'>" + value + "</span>";
+        } else if (rnd === 5) {
+          spans = spans + "<span class='" + cls + " rot l5'>" + value + "</span>";
+        } else if (rnd === 6) {
+          spans = spans + "<span class='" + cls + " rot l4'>" + value + "</span>";
+        } else if (rnd === 7) {
+          spans = spans + "<span class='" + cls + " rot l3'>" + value + "</span>";
+        } else if (rnd === 8) {
+          spans = spans + "<span class='" + cls + " rot l2'>" + value + "</span>";
+        } else if (rnd === 9) {
+          spans = spans + "<span class='" + cls + " rot l1'>" + value + "</span>";
+        } else {
+          spans = spans + "<span class='" + cls + "'>" + value + "</span>";
+        }
       }
     }
   );
